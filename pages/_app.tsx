33c730220e96a598eaeef22a21eaebd5efda285c
Kunjo Josue Lee Ramirez <kunjo.lee@telusinternational.com>
2022-10-11 17:47:49 +0000

    Merged in material-ui-setup (pull request #3)
    Material ui setup
    
    * feat: readme test added
    
    * Merged in ts-config (pull request #1)
    
    Ts config implementation
    
    * feat: readme test added
    
    * feat: migration from javascript to typescript
    
    Approved-by: Victor Morales
    
    * feat: material-ui config
    
    * fix: pull conflicts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app';
 
+
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import '../styles/globals.css';
 
+const basicTheme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ basicTheme }>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
